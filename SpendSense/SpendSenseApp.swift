504ebbcd9fce9ea44f3443358304c24d60f50313
Nimrat Khalsa <nimratkhalsa16@icloud.com>
2023-07-10 19:44:02 +0530

    Initial Commit

---
+++ SpendSense/SpendSenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpendSenseApp.swift
+//  SpendSense
+//
+//  Created by Nimrat Khalsa on 10/07/23.
+//
+
+import SwiftUI
+
+@main
+struct SpendSenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
